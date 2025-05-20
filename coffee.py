e241969fb372115f36f79ef3656f505073d2dda5
Ezra Oduor <ezrachurchill20@gmail.com>
2025-05-20 19:06:48 +0300

    adding the object relations to class coffee

--- coffee.py
+++ coffee.py
@@ -1,12 +1,25 @@
 class Coffee:
-
     all = []
 
     def __init__(self, name):
         self.name = name
-        if not isinstance(name, str):
-            raise TypeError("the name must be a string")
-        if  len(name) < 3:
-            raise ValueError("name must be atleast 3 characters long")
-        self._name = name
-        Coffee._all.append(self)
+        Coffee.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Coffee name must be a string")
+        if not value:
+            raise ValueError("Coffee name cannot be empty")
+        self._name = value
+
+    def orders(self):
+        from order import Order
+        return [order for order in Order.all if order.coffee == self]
+
+    def customers(self):
+        return list({order.customer for order in self.orders()})
--- customer.py
+++ customer.py
@@ -1,8 +1,3 @@
-
-from order import Order
-from order import Order
-
-
 class Customer:
     all = []
 
@@ -19,14 +14,16 @@ class Customer:
         if not isinstance(value, str):
             raise TypeError("Name must be a string")
         if not 1 <= len(value) <= 15:
-            raise ValueError("Name must be 1-15 characters")
+            raise ValueError("Name must be 1–15 characters long")
         self._name = value
 
     def orders(self):
+        from order import Order
         return [order for order in Order.all if order.customer == self]
 
     def coffees(self):
         return list({order.coffee for order in self.orders()})
 
     def new_order(self, coffee, price):
-        return order(self,coffee,price)
+        from order import Order
+        return Order(self, coffee, price)
